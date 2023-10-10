e86bd4c19f055384704bba26c1f2f7a73b202ad4
stuxnot <41650734+Stuxnot@users.noreply.github.com>
2023-10-10 14:57:33 +0200

    Add python bindings

---
+++ bindings/python/pynyxstone.cpp
@@ -0,0 +1,78 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+#include <sstream>
+#include <iomanip>
+
+#include <nyxstone.h>
+
+namespace py = pybind11;
+
+std::vector<uint8_t> assemble_to_bytes(Nyxstone& nyxstone, std::string assembly, uint64_t address, std::vector<Nyxstone::LabelDefinition> labels) {
+    std::vector<uint8_t> bytes;
+    nyxstone.assemble_to_bytes(assembly, address, labels, bytes);
+    return bytes;
+}
+
+std::vector<Nyxstone::Instruction> assemble_to_instructions(Nyxstone& nyxstone, std::string assembly, uint64_t address, std::vector<Nyxstone::LabelDefinition> labels) {
+    std::vector<Nyxstone::Instruction> instructions;
+    nyxstone.assemble_to_instructions(assembly, address, labels, instructions);
+    return instructions;
+}
+
+std::string disassemble_to_text(Nyxstone& nyxstone, std::vector<uint8_t> bytes, uint64_t address, uint64_t count) {
+    std::string assembly;
+    nyxstone.disassemble_to_text(bytes, address, count, assembly);
+    return assembly;
+}
+
+std::vector<Nyxstone::Instruction> disassemble_to_instructions(Nyxstone& nyxstone, std::vector<uint8_t> bytes, uint64_t address, uint64_t count) {
+    std::vector<Nyxstone::Instruction> instructions;
+    nyxstone.disassemble_to_instructions(bytes, address, count, instructions);
+    return instructions;
+}
+
+PYBIND11_MODULE(nyxstone, m) {
+    m.doc() = "pybind11 plugin for nyxstone";
+
+    py::class_<Nyxstone::LabelDefinition>(m, "LabelDefinition")
+        .def(py::init<const std::string&, const uint64_t&>())
+        .def_readwrite("name", &Nyxstone::LabelDefinition::name)
+        .def_readwrite("address", &Nyxstone::LabelDefinition::address);
+
+    py::class_<Nyxstone::Instruction>(m, "Instruction")
+        .def_readwrite("address", &Nyxstone::Instruction::address)
+        .def_readwrite("bytes", &Nyxstone::Instruction::bytes)
+        .def_readwrite("assembly", &Nyxstone::Instruction::assembly)
+        .def("__repr__", [](const Nyxstone::Instruction& i) { 
+            std::stringstream out;
+            out << "<address: 0x" << std::hex << std::setw(8) << std::setfill('0') << i.address << ", assembly: \"" << i.assembly << "\", bytes: [ ";
+            for (const auto& b : i.bytes) {
+                out << std::hex << std::setw(2) << std::setfill('0') << static_cast<uint32_t>(b) << " ";
+            }
+            out << "]>";
+            return out.str();
+        })
+    ;
+
+    py::class_<NyxstoneBuilder> builder(m, "NyxstoneBuilder");
+
+    py::enum_<NyxstoneBuilder::IntegerBase>(builder, "IntegerBase")
+        .value("Dec", NyxstoneBuilder::IntegerBase::Dec, "Decimal printing")
+        .value("HexPrefix", NyxstoneBuilder::IntegerBase::HexPrefix, "Hex, prefixed with '0x'")
+        .value("HexSuffix", NyxstoneBuilder::IntegerBase::HexSuffix, "Hex, suffixed with 'h'");
+
+    builder.def(py::init())
+        .def("with_triple", &NyxstoneBuilder::with_triple, "Specify the llvm target triple")
+        .def("with_features", &NyxstoneBuilder::with_features, "Specify the llvm features to be en- or disabled")
+        .def("with_cpu", &NyxstoneBuilder::with_cpu, "Specify the cpu to use")
+        .def("with_immediate_style", &NyxstoneBuilder::with_immediate_style, "Specify the style in which immediates are printed")
+        .def("build", &NyxstoneBuilder::build, "Build the Nyxstone instance");
+
+
+    py::class_<Nyxstone>(m, "Nyxstone")
+       .def("assemble_to_bytes", &assemble_to_bytes, py::arg("assembly"), py::arg("address"), py::arg("labels"))
+       .def("assemble_to_instructions", &assemble_to_instructions, py::arg("assembly"), py::arg("address"), py::arg("labels"))
+       .def("disassemble_to_instructions", &disassemble_to_instructions, py::arg("bytes"), py::arg("address"), py::arg("count"))
+       .def("disassemble_to_text", &disassemble_to_text, py::arg("bytes"), py::arg("address"), py::arg("count"));
+}
---
+++ bindings/python/setup.py
@@ -0,0 +1,126 @@
+from glob import glob
+from setuptools import setup
+import os
+import subprocess
+
+try:
+    from pybind11.setup_helpers import Pybind11Extension
+except ImportError:
+    from setuptools import Extension as Pybind11Extension
+
+LLVM_PREFIX_NAME = "NYXSTONE_LLVM_PREFIX"
+
+
+class ValidLLVMConfig:
+    def __init__(self):
+        llvm_config = (
+            os.path.join(os.getenv(LLVM_PREFIX_NAME), "bin/llvm-config")
+            if os.getenv(LLVM_PREFIX_NAME) != None
+            else "llvm-config"
+        )
+
+        try:
+            version = subprocess.run(
+                [llvm_config, "--version"], capture_output=True
+            ).stdout.decode("utf-8")
+        except FileNotFoundError as e:
+            print(f"Could not find llvm-config in ${LLVM_PREFIX_NAME} or $PATH")
+            exit(1)
+
+        major_version = version.split(".")[0]
+
+        if major_version != "15":
+            print(f"LLVM Major version must be 15, found {major_version}!")
+            exit(1)
+
+        lib_output = subprocess.run(
+            [
+                llvm_config,
+                "--link-static",
+                "--libs",
+            ],
+            capture_output=True,
+        )
+
+        if lib_output.returncode != 0:
+            print(
+                "Cannot link statically, please install LLVM with static linking support"
+            )
+            exit(1)
+
+        self.config = llvm_config
+
+    def get_llvm_libraries(self) -> (list[str], str):
+        lib_output = subprocess.run(
+            [
+                self.config,
+                "--link-static",
+                "--system-libs",
+                "--libs",
+                "core",
+                "mc",
+                "all-targets",
+            ],
+            capture_output=True,
+        )
+
+        libraries = (
+            lib_output.stdout.decode("utf-8")
+            .replace("\n", " ")
+            .strip()
+            .replace("-l", "")
+            .split(" ")
+        )
+
+        lib_location = (
+            subprocess.run([self.config, "--ldflags"], capture_output=True)
+            .stdout.decode("utf-8")
+            .replace("\n", "")  # remove trailing '\n'
+            .replace("-L", "")  # remove leading -L re-added by pybind
+            .strip()  # strip trailing whitespace
+        )
+
+        return libraries, lib_location
+
+    def get_llvm_include_dir(self) -> str:
+        llvm_include_dir = (
+            subprocess.run([self.config, "--includedir"], capture_output=True)
+            .stdout.decode("utf-8")
+            .replace("\n", "")
+            .strip()
+        )
+
+        return llvm_include_dir
+
+
+llvm = ValidLLVMConfig()
+llvm_libs, llvm_lib_dir = llvm.get_llvm_libraries()
+llvm_inc_dir = llvm.get_llvm_include_dir()
+
+nyxstone_srcs = glob("nyxstone/src/*.cpp")
+
+binder = ["pynyxstone.cpp"]
+
+srcs = sorted(nyxstone_srcs + binder)
+
+desc = "Python bindings for the nyxstone library."
+
+ext_modules = [
+    Pybind11Extension(
+        name="nyxstone",
+        sources=srcs,
+        include_dirs=["nyxstone/include/", "nyxstone/src/", llvm_inc_dir],
+        libraries=llvm_libs,
+        library_dirs=[llvm_lib_dir],
+        extra_link_args=["-static-libstdc++", "-static-libgcc"] # try to reduce dependencies
+    )
+]
+
+setup(
+    name="nyxstone",
+    version="0.0.0",
+    description=desc,
+    author="emproof GmbH",
+    author_email="info@emproof.de",
+    ext_modules=ext_modules,
+)
