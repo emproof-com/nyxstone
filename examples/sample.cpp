997ac4a9f3af93931c4faa34037eacd8637c06f3
stuxnot <41650734+Stuxnot@users.noreply.github.com>
2023-10-25 16:30:56 +0200

    Removes std::endl

--- examples/sample.cpp
+++ examples/sample.cpp
@@ -16,7 +16,7 @@ int main(int /* argc */, char** /* argv */) {
         std::vector<Nyxstone::Instruction> instructions;
         std::string disassembly;
 
-        std::cout << "assemble_to_bytes:" << std::endl;
+        std::cout << "assemble_to_bytes:\n";
         std::cout << "\tmov rax, rax : [ ";
         nyxstone_x86_64->assemble_to_bytes("mov rax, rax", 0x1000, labels, bytes);
         std::cout << std::hex;
@@ -24,7 +24,7 @@ int main(int /* argc */, char** /* argv */) {
             std::cout << std::setfill('0') << std::setw(2) << static_cast<uint32_t>(byte) << " ";
         }
         std::cout << std::dec;
-        std::cout << "] - expected [ 48 89 c0 ]" << std::endl;
+        std::cout << "] - expected [ 48 89 c0 ]\n";
 
         std::cout << "\tbne .label : [ ";
         nyxstone_armv8m->assemble_to_bytes("bne .label", 0x1000, labels, bytes);
@@ -34,7 +34,7 @@ int main(int /* argc */, char** /* argv */) {
             std::cout << std::setfill('0') << std::setw(2) << static_cast<uint32_t>(byte) << " ";
         }
         std::cout << std::dec;
-        std::cout << "] - expected [ 06 d1 ]" << std::endl;
+        std::cout << "] - expected [ 06 d1 ]\n";
 
         std::cout << std::endl << "assemble_to_instructions:" << std::endl;
         std::cout << "\tmov rax, rax : [ ";
@@ -46,7 +46,7 @@ int main(int /* argc */, char** /* argv */) {
             }
             std::cout << std::dec;
         }
-        std::cout << "] - expected [ 48 89 c0 ]" << std::endl;
+        std::cout << "] - expected [ 48 89 c0 ]\n";
 
         std::cout << "\tbne .label : [ ";
         nyxstone_armv8m->assemble_to_instructions("bne .label", 0x1000, labels, instructions);
@@ -57,23 +57,23 @@ int main(int /* argc */, char** /* argv */) {
             }
             std::cout << std::dec;
         }
-        std::cout << "] - expected [ 06 d1 ]" << std::endl;
+        std::cout << "] - expected [ 06 d1 ]\n";
 
         std::cout << std::endl << "disassemble_to_text:" << std::endl;
         std::cout << "\t48 89 c0 : [ ";
         nyxstone_x86_64->disassemble_to_text({0x48, 0x89, 0xc0}, 0x1000, 0, disassembly);
         disassembly.erase(disassembly.find_last_not_of(" \t\n\r") + 1);
         std::cout << disassembly;
-        std::cout << " ] - expected [ mov rax, rax ]" << std::endl;
+        std::cout << " ] - expected [ mov rax, rax ]\n";
 
         std::cout << "\t06 d1 : [ ";
         nyxstone_armv8m->disassemble_to_text({0x06, 0xd1}, 0x1000, 0, disassembly);
         disassembly.erase(disassembly.find_last_not_of(" \t\n\r") + 1);
         std::cout << disassembly;
-        std::cout << " ] - expected [ bne #12 ]" << std::endl;
+        std::cout << " ] - expected [ bne #12 ]\n";
 
     } catch (const std::exception& e) {
-        std::cout << "An error occured: " << e.what() << std::endl;
+        std::cout << "An error occured: " << e.what() << "\n";
         return 1;
     }
 
