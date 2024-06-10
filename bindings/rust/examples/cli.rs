97122cba33cded5a9a80ff4876641f5f7507ccac
stuxnot <41650734+stuxnot@users.noreply.github.com>
2024-06-10 13:14:45 +0200

    Add docs and cpu/feature flags

--- bindings/rust/examples/cli.rs
+++ bindings/rust/examples/cli.rs
@@ -12,8 +12,11 @@ use nyxstone::{Instruction, IntegerBase, Nyxstone, NyxstoneConfig};
 #[derive(Clone, Subcommand)]
 enum Command {
     Assemble {
-        #[arg(short, long, value_parser = parse_label)]
+        /// Custom label address pairs, formatted like "label=<address>". Multiple labels should be specified by using
+        /// this flag multiple times, i.e. -l "label0=<address0>" -l "label1=<address1>"
+        #[arg(short, long = "label", value_parser = parse_label)]
         labels: Option<Vec<(String, u64)>>,
+
         assembly: String,
     },
     Disassemble {
@@ -22,13 +25,26 @@ enum Command {
     },
 }
 
+/// A
 #[derive(Parser)]
 struct Args {
+    /// Architecture LLVM triple or architecture identifier of triple, for example "x86_64", "x86_64-linux-gnu",
+    /// "armv8", "armv8eb", "thumbv8", "aarch64"
     #[arg(short = 'a', long)]
     architecture: String,
 
+    /// Start address of assembly/disassembly
     #[arg(short = 's', long, value_parser = parse_maybe_hex, default_value_t = 0)]
     address: u64,
+
+    /// LLVM cpu specifier, refer to `llc -march=ARCH -mcpu=help` for a comprehensive list
+    #[arg(short = 'c', long)]
+    cpu: Option<String>,
+
+    /// LLVM features to enable/disable, comma seperated feature strings prepended by '+' or '-' to enable or disable
+    /// respectively. Refer to `llc -march=ARCH -mattr=help` for a comprehensive list
+    #[arg(short = 'f', long)]
+    features: Option<String>,
 
     #[command(subcommand)]
     command: Command,
@@ -49,8 +65,9 @@ fn main() -> Result<()> {
     let nyxstone = Nyxstone::new(
         &cli.architecture,
         NyxstoneConfig {
+            cpu: &cli.cpu.unwrap_or_default(),
+            features: &cli.features.unwrap_or_default(),
             immediate_style: IntegerBase::HexPrefix,
-            ..Default::default()
         },
     )?;
 
